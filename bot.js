301b4b7d44695e128399a04a9b36dbdc839ec5d1
dtcholak <d.tcholakov@gmail.com>
2016-03-05 15:01:22 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,11 +5,11 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = "Dimi Tcholakov";
+      botRegex = /^\/cool guy$/;
       botRegexa = /^\/wall/;
-      botRegext = /^\/test/;
+      botRegext = /\stest/;
 
-  if(request.text && botRegex.test("name")) {
+  if(request.text && botRegex.test(request.text))) {
     this.res.writeHead(200);
     postMessage("dimi");
     this.res.end();
