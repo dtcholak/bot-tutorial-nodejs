3af7c9100bfceff0da27df7d83bdbd26f73500f6
dtcholak <d.tcholakov@gmail.com>
2016-03-05 14:04:59 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,8 +5,8 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/cool face$/;
-      botRegexa = /^\ wall$/;
+      botRegex = /^\/cool face/;
+      botRegexa = /^\/wall/;
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
