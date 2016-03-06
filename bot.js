630aecb59ac8a4c1ac1a14276c266e70040ca0c0
dtcholak <d.tcholakov@gmail.com>
2016-03-05 19:49:49 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,8 @@ var botID = process.env.BOT_ID;
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegex = /^\/cool guy$/;
-      var wall = new RegExp(" wall "|"wall ");
-      var ping = /(ping | ping )/i;
+      var wall = /(wall | wall | wall|wall)/i;
+      var ping = /(ping | ping | ping|ping)/i;
       
 if(request.name == "Dillan Nayee") {
     this.res.writeHead(200);
